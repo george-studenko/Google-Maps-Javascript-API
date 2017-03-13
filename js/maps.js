a374c40d82161171c7b8fbd2982eaf0aaae95fb0
George Studenko <gnstudenko@gmail.com>
2017-03-13 17:36:58 +0100

    feat Add makersViewModel

--- js/maps.js
+++ js/maps.js
@@ -9,6 +9,16 @@ function MapMarker(address, title, description){
   this.lng=lng;
 }
 
+var markersViewModel = function(){
+  this.markers = [
+        new MapMarker("Carrer de Sant Miquel 115, Barcelona","Home","This is Home"),
+        new MapMarker("Carrer de Sant Miquel 110, Barcelona","Home 1","This is Home"),
+        new MapMarker("Carrer del Mar 15, Barcelona","Home 2","This is Home"),
+        new MapMarker("Carrer Judici 15","Home 3","This is Home"),
+        new MapMarker("Carrer Judici 5","Home 4","This is Home")
+    ];
+};
+
 function initMap(){
   var styles = [
     {
