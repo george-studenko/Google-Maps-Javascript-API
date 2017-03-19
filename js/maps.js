d45ada4793cd28cc21ea721fc797669c453ee196
George Studenko <gnstudenko@gmail.com>
2017-03-19 22:45:43 +0100

    refactor: Increase wikiNearByThumbnails radius

--- js/maps.js
+++ js/maps.js
@@ -4,7 +4,7 @@ var allMarkers = [];
 var allInfoWindows = [];
 var currentInfoWindow;
 var bounds = new google.maps.LatLngBounds();
-var wikiNearbyThumbnails = 'https://en.wikipedia.org/w/api.php?action=query&prop=coordinates%7Cpageimages%7Cpageterms&colimit=50&piprop=thumbnail&pithumbsize=270&pilimit=50&wbptterms=description&generator=geosearch&ggscoord=41.3766803%7C2.1873975&ggsradius=500&ggslimit=50&format=json';
+var wikiNearbyThumbnails = 'https://en.wikipedia.org/w/api.php?action=query&prop=coordinates%7Cpageimages%7Cpageterms&colimit=50&piprop=thumbnail&pithumbsize=270&pilimit=50&wbptterms=description&generator=geosearch&ggscoord=41.3766803%7C2.1873975&ggsradius=800&ggslimit=50&format=json';
 var wikiNearbyInfo = 'https://en.wikipedia.org/w/api.php?action=opensearch&prop=revisions&format=json&search=#SEARCH#';
 
  var MapMarker = function (address, title, description, visible, map, lat, lon, type){
