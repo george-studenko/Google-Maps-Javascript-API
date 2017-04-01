d32f6b5124fa88e070c81a6c0f5d0bc9d732a75f
George Studenko <gnstudenko@gmail.com>
2017-04-01 19:55:26 +0200

    refactor: Change wikiNearbyThumbnails call url

--- js/maps.js
+++ js/maps.js
@@ -2,7 +2,7 @@ var map;
 var polygon= null;
 var currentInfoWindow;
 var bounds = new google.maps.LatLngBounds();
-var wikiNearbyThumbnails = 'https://en.wikipedia.org/w/api.php?action=query&prop=coordinates%7Cpageimages%7Cpageterms&colimit=50&piprop=thumbnail&pithumbsize=270&pilimit=50&wbptterms=description&generator=geosearch&ggscoord=41.3766803%7C2.1873975&ggsradius=500&ggslimit=50&format=json';
+var wikiNearbyThumbnails = 'https://en.wikipedia.org/w/api.php?action=query&prop=extracts%7Ccoordinates%7Cpageimages%7Cpageterms&colimit=50&piprop=thumbnail&pithumbsize=270&pilimit=50&wbptterms=description&generator=geosearch&ggscoord=41.3766803%7C2.1873975&ggsradius=500&ggslimit=50&format=json&rvprop=content';
 var wikiNearbyInfo = 'https://en.wikipedia.org/w/api.php?action=opensearch&prop=revisions&format=json&search=#SEARCH#';
 
 var weatherView = {
