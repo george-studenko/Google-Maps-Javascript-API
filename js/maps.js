ff29a43db72a3d747f8ace8f5c04ee1f2d27d085
George Studenko <gnstudenko@gmail.com>
2017-04-02 22:04:42 +0200

    feat: Add capitalize function

--- js/maps.js
+++ js/maps.js
@@ -23,6 +23,10 @@ var currentInfoWindow;
 var wikiNearbyThumbnails = 'https://en.wikipedia.org/w/api.php?action=query&prop=coordinates%7Cpageimages%7Cpageterms&colimit=50&piprop=thumbnail&pithumbsize=270&pilimit=50&wbptterms=description&generator=geosearch&ggscoord=41.3766803%7C2.1873975&ggsradius=800&ggslimit=50&format=json';
 var wikiNearbyInfo = 'https://en.wikipedia.org/w/api.php?action=opensearch&prop=revisions&format=json&search=#SEARCH#';
 
+function capitalize(word){
+  return word.charAt(0).toUpperCase() + word.slice(1);
+}
+
 var weatherView = {
   currentWeatherElement : $('.currentWeather')
 }
